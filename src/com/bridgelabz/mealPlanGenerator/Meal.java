fb6b87b138466953ddfa8470710cbb9124c5a887
Akki <chauhanakshit61@gmail.com>
2025-03-27 22:46:49 +0530

    [Akshit] ADD: Managing Meals

---
+++ src/com/bridgelabz/mealPlanGenerator/Meal.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.mealPlanGenerator;
+
+
+// Generic class to manage meal plans
+public class Meal<T extends MealPlan> {
+    private T mealType;
+
+    public Meal(T mealType){
+        this.mealType = mealType;
+    }
+
+    public void showMealDetails(){
+        mealType.displayMeal();
+    }
+}
